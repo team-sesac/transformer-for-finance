6486bd1f08d6a40870a6b6f3a6ee92cedf91019d
Seyeon <seyeon47@naver.com>
2023-12-08 14:06:01 +0900

    feat: data generator v1

--- data/create_dataset/data_generator.py
+++ data/create_dataset/data_generator.py
@@ -1,7 +1,7 @@
 import pandas as pd
-
 import utils as u
 
+# TA 지표 포함 전체 columns 이름 목록
 # ['Open', 'High', 'Low', 'Close', 'Volume', 'Change', 'pct_change',
 #        'volume_adi', 'volume_obv', 'volume_cmf', 'volume_fi', 'volume_em',
 #        'volume_sma_em', 'volume_vpt', 'volume_vwap', 'volume_mfi',
@@ -29,28 +29,62 @@ import utils as u
 #        'momentum_pvo_hist', 'momentum_kama', 'others_dr', 'others_dlr',
 #        'others_cr']
 
-def get_dataset_for_transformer():
-    # data_list = u.get_KRX_list()
-    # 005380 현대자동차
-    # 000270 기아
-    to_include = ['Open', 'High', 'Low', 'Close', 'Volume', 'Change', 'pct_change',
-                  'trend_macd_diff', 'trend_sma_fast', 'trend_ema_fast', 'volume_obv',
-                  'momentum_rsi', 'trend_cci', 'volatility_bbh']
 
-    df1 = u.get_single_ticker(symbol='005380',
+def get_stock_codes():
+    return u.get_KRX_list().reset_index(drop=True)
+
+
+def get_dataset_for_transformer(idx, ticker):
+    '''
+    1개 종목의 2010년~2023년 동안의 주가 + 일부 ta지표 데이터셋을 csv로 저장하는 코드
+    ticker.Code = '005380'
+    ticker.Name = '삼성전자'
+    005380 현대자동차, 000270 기아
+    '''
+
+    # 선택할 지표가 있는경우 to_include 주석 해제 (선택 안하면 모든 ta 지표가 데이터셋에 저장됨)
+    # to_include = ['Open', 'High', 'Low', 'Close', 'Volume', 'Change', 'pct_change',
+    #               'trend_macd_diff', 'trend_sma_fast', 'trend_ema_fast', 'volume_obv',
+    #               'momentum_rsi', 'trend_cci', 'volatility_bbh']
+
+    # 1개 종목의 데이터 가져오기
+    df = u.get_single_ticker(symbol=ticker.Code,
                               start_date='20100101',
-                              end_date='20231130')
-    df1 = u.cat_ta(df1, to_include)
+                              end_date='20231130').reset_index()
+    try:
+        df = u.cat_ta(df)
+        # 선택지표만 추출하려면 아래것 주석해제
+        # df = u.cat_ta(df, to_include)
+    except ValueError:
+        return
 
-    df2 = u.get_single_ticker(symbol='000270',
-                              start_date='20100101',
-                              end_date='20231130')
-    df2 = u.cat_ta(df2, to_include)
-    fin_df = pd.concat([df1, df2], axis=1)
+    # 날짜 빼고 모든 컬럼 datatype float로 바꾸기
+    columns_to_convert = df.columns.to_list()
+    columns_to_convert.remove('Date')
+    df[columns_to_convert] = df[columns_to_convert].astype(float)
 
-    u.export_csv(fin_df, '../hyundai_kia_2010.csv')
+    # 종목 이름 첫번재 줄에 쓰기
+    df.insert(loc=1, column='Ticker', value=ticker.Name)
+
+    # 상위폴더의 tf_dataset에 01_삼성전자_2010.csv 파일명으로 저장하기
+    u.export_csv(df, f'../tf_dataset/{idx}_{ticker.Name}_2010.csv')
+
+
+def save_all_stock_data():
+    '''한국 상장 종목별로 10개년 일일 데이터(ta 전체포함)셋 csv 만들기'''
+    stocks = get_stock_codes()
+    # stocks = pd.read_csv('../stock_list.csv')
+    # stocks = stocks.iloc[1207:, :]
+    for idx, item in enumerate(stocks.itertuples()):
+        # idx += 1207
+        try:
+            get_dataset_for_transformer(idx, item)
+        except:
+            continue
+        if idx % 10 == 0:
+            print(f"saved ~ {idx} {item.Name}")
 
 
 if __name__ == '__main__':
-    get_dataset_for_transformer()
+    save_all_stock_data()
 
